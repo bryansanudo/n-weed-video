257820564397ad5b40e221ef7077ddaf4b6386ab
Bryan <bryansanudo95@gmail.com>
2023-04-11 16:31:42 -0500

    shorts icon

--- src/components/categorys/Navigate.jsx
+++ src/components/categorys/Navigate.jsx
@@ -8,9 +8,11 @@ const Navigate = () => {
   return (
     <div className="flex  lg:flex-row p-5 mb-12 gap-5 items-center justify-center ">
       <Link to="/gelam" onClick={useScroll}>
-        <div className="w-32 text-center rounded-3xl shadow-lg shadow-[#ffcdc2] hover:scale-105 duration-200  px-6 p-3 cursor-pointer  ">
-          Shorts
-        </div>
+        <img
+          src="/shorts.svg"
+          alt=""
+          className="w-20 hover:scale-105 duration-300 animate-pulse "
+        />
       </Link>
       {/* <Link to="/dyel" onClick={useScroll}>
         <div className="w-16 text-center  rounded-3xl shadow-lg shadow-[#ffcdc2] hover:scale-105 duration-200  px-6 p-3 cursor-pointer  ">
